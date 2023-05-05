35c85aba777ddea0aa2040bc17b99e57430a970c
Emmanuel Vitor <emmanuelvitor.ti@gmail.com>
2023-05-05 11:49:44 -0300

    feat: explicação sobre algumas funções aritiméticas do php

--- base/introducao/funcoes_aritimeticas.php
+++ base/introducao/funcoes_aritimeticas.php
@@ -1,11 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
     <?php 
     echo "<h2> Estudo sobre valor absoluto </h2>";
@@ -55,7 +57,7 @@
     echo "<strong>round de 2.5: </strong> $round2 <br>";
     echo "<strong>round de 2.52: </strong> $round2 <br>";
 
-    echo "<br><br>";
+    echo "<br>";
     echo "<p>
             <strong>
             Calcula a hipotenusa:
@@ -76,7 +78,7 @@
             <strong>intdiv </strong> --> Calcula o valor da divisão inteira 
          </p>";
 
-         echo "<br><br>";
+         echo "<br>";
          echo "<p>
          <strong>
          IntDiv
@@ -86,7 +88,7 @@
          echo "<strong>A divisão inteira do intdiv 5 e 2 = </strong> $intDiv";
 
 
-         echo "<br><br>";
+         echo "<br>";
          echo "<p>
                  <strong>
                  Funções min(0 e max():
@@ -97,6 +99,85 @@
                     <strong>min()</strong> --> Localiza o maior valor · count() - Conta o número de elementos de uma variável, ou propriedades de um objeto.<br>
                     <strong>max()</strong> --> Retorna o maior valor do array. Se no mínimo dois parâmetros são fornecidos, max() retornará o maior 
                 </p>";
+                $min =  min(2,5);
+                $min2 = min(2, 10, 14, 0, 5, 7, 3, -3);
+                $max = max(3, 7);
+                $max2 =  max(2, 10, 14, 0, 5, 7, 3, -3);
+
+                echo "<strong>min: </strong> $min <br>";
+                echo "<strong>min: </strong> $min2 <br>";
+                echo "<strong>max: </strong> $max <br>";
+                echo "<strong>max: </strong> $max2 <br>";
+
+        echo "<br>";
+        echo "<p>
+                <strong>
+                PI:
+                </strong>
+            </p>";
+                $pi =  pi();
+                $pi2 = pi()*2;
+                $piPorConstante = M_PI;
+                echo "<strong>Pi: </strong> $pi <br>";
+                echo "<strong>Pi 2: </strong> $pi2<br>";
+                echo "<strong>Pi atraves de constante: </strong> $piPorConstante<br>";
+
+                echo "<br>";
+                echo "<p>
+                        <strong>
+                        Funções  de seno, cosseno e tangente:
+                        </strong>
+                    </p>";
+                    
+                echo "
+                        <p>
+                            <strong>sin()</strong> <br>
+                            <strong>cos()</strong> <br>
+                            <strong>tang()</strong> 
+                        </p>";
+                        $sin =  sin(30);
+                        $cos =  cos(30);
+                        $tang = tan(30);
+                        
+                        echo "<strong>Seno de 30: </strong> $sin <br>";
+                        echo "<strong>cosseno de 30: </strong> $cos <br>";
+                        echo "<strong>tangente de 30: </strong> $tang <br>";  
+
+        echo "<br>";
+        echo "<p>
+                <strong>
+              POW - Potenciação
+                </strong>
+            </p>";
+            echo "
+            <p>
+                <strong>*</strong> --> Diferença~de usar função de potenciação (pow()) para usar o sinal ( ** ) é <u>somente</u> na ordem de precedencia nos calculos
+            </p>";
+                $pow =  pow(2);           
+                echo "<strong>Potencia de 2: </strong> $pow<br>";
+
+        echo "<br>";
+        echo "<p>
+                <strong>
+                Funções  de seno, cosseno e tangente:
+                </strong>
+            </p>";
+            
+        echo "
+                <p>
+                    <strong>sin()</strong> <br>
+                    <strong>cos()</strong> <br>
+                    <strong>tang()</strong> 
+                </p>";
+                $sin =  sin(30);
+                $cos =  cos(30);
+                $tang = tang(30);
+                
+                echo "<strong>Seno de 30: </strong> $sin <br>";
+                echo "<strong>cosseno de 30: </strong> $sos <br>";
+                echo "<strong>tangente de 30: </strong> $tang <br>";     
+     
     ?>
 </body>
+
 </html>
