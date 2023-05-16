b35fa057b5f90dee9611c8ba63933d2106e3c94f
Emmanuel Vitor <emmanuelvitor.ti@gmail.com>
2023-05-16 11:53:14 -0300

    links e dicas

---
+++ base/subrotinas/valor_referencia.php
@@ -0,0 +1,40 @@
+<?php 
+
+
+include 'funcoes.php';
+
+$todasAsContas = [
+    "528.319.050-10"=>[
+        "titular" => "Ana Silva",
+        "saldo" => 2500.00,
+        "conta" => "00123-4"
+    ],
+
+    "192.292.880-12"=>[
+        "titular" => "João Souza",
+        "saldo" => 5000.00,
+        "conta" => "01234-5"
+    ],
+
+    "950.318.310-30"=>[
+        "titular" => "Maria Santos",
+        "saldo" => 10000.00,
+        "conta" => "12345-6"
+    ],
+
+    "785.198.880-20"=>[
+        "titular" => "Pedro Oliveira",
+        "saldo" => 3500.00,
+        "conta" => "23456-7"
+    ],
+
+    "293.429.000-03"=>[
+        "titular" => "Luciana Costa",
+        "saldo" => 7500.00,
+        "conta" => "34567-8"
+    ]
+];
+
+// https://hcode.com.br/blog/passagem-de-parametros-por-valor-e-referencia-no-php/
+// video 01 do arquivo : 05 - Referências e mais listas
+?>
