1b2e2624adaefc6ec41921dc35aabe4c43248a62
Emmanuel Vitor <emmanuelvitor.ti@gmail.com>
2023-05-17 10:58:43 -0300

    feat:  funções de ordenação, verificar se existe e gerar posção em array

--- base/array/funções_de_array.php
+++ base/array/funções_de_array.php
@@ -14,7 +14,6 @@ $studentGrades = array(
     "Emma" => 89
 );
 
-
 // function range($start, $end, int|float $step = 1): array
 # onde é definido  inicio (start), fim (end) e um parametro opcional que delimita d
 # e qnto em quanto vai ser gerado valor (step)
@@ -23,5 +22,45 @@ $arr = range(1,10);
 foreach ($arr as $item) {
     echo $item;
 }
+
+echo PHP_EOL;
+
+#verificar se existe um elemento no array
+#in_array: Checks if a value exists in an array (return a bool)
+if (in_array("Jacob",$studentGrades)) {
+    echo "Existe";
+} else {
+    echo "Não Existe";
+}
+echo PHP_EOL;
+
+echo  var_dump((in_array("jose",$studentGrades)));
+
+
+#verificar se o array existe e qual sua posição
+#Searches the array for a given value and returns the first corresponding key if successful
+$posicao = array_search("2",$numbers);
+echo ($posicao);
+
+#organizar (ordenar) elementos de um array
+sort(($studentGrades));
+print_r($studentGrades);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 ?>
