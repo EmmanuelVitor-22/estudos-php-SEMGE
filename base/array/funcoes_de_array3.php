888ccdb55408655c69e047a71084bb1d78f48da2
Emmanuel Vitor <emmanuelvitor.ti@gmail.com>
2023-05-17 11:51:56 -0300

    feat: cria terceiro arquivo para funções em array, para focar em outros tipos, como funções de ordenação

---
+++ base/array/funcoes_de_array3.php
@@ -0,0 +1,20 @@
+<?php 
+
+$arr = [
+    "uva",
+    "goiaba",
+    "maça"
+];
+
+$clientes = [
+    "123.456.789-10" => [
+        "nome" => "João da Silva",
+        "telefone" => "(11) 1234-5678"
+    ],
+    "987.654.321-00" => [       
+        "nome" => "Maria Souza",
+        "telefone" => "(22) 9876-5432"
+    ]
+];
+
+?>
