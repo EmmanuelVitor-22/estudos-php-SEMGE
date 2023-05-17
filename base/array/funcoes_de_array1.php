605a73e2f268124bee65f79ff6642c93df129097
Emmanuel Vitor <emmanuelvitor.ti@gmail.com>
2023-05-17 11:49:54 -0300

    feat: continau explicação sobre funções de array

--- base/array/funcoes_de_array1.php
+++ base/array/funcoes_de_array1.php
@@ -23,8 +23,49 @@ echo PHP_EOL;
 #ps:  ela valida até valores nulos
 var_dump(array_key_exists(4,$arr));
 
+echo PHP_EOL;
+echo PHP_EOL;
 //VERIFICAR ALGUMAS FUNÇÕES MAIS ATUAIS (MELHORES USADAS EM ARRAY ASSOCIATIVO)
+//inicio
+$clientes = [
+    "123.456.789-10" => [
+        "nome" => "João da Silva",
+        "telefone" => "(11) 1234-5678"
+    ],
+    "987.654.321-00" => [       
+        "nome" => "Maria Souza",
+        "telefone" => "(22) 9876-5432"
+    ]
+];
+
+#verificar primeira posição:
+   echo "Primeira chave:  " . array_key_first($clientes);
+   echo PHP_EOL;
+   echo "Ultima chave:  " . array_key_last($clientes);
+
+//fim 
+
+#inverter chaves e valores dos arrays
+echo PHP_EOL;
+print_r(array_flip($arr));
+echo PHP_EOL;
 
 
+#separando array por algum tipo de separador 
+#$ implod:Join array elements with a string; Basicamente você tranforma o array em uma string
+//recebe dois parametros, o separador e o array
+echo implode(' - ', $arr);
+echo PHP_EOL;
 
+#converter uma string num array
+// criar array a partir de string
+// o separador some da string, nessa função
+print_r( explode(' ', "teste"));
+echo PHP_EOL;
+print_r( explode(' ', "teste de pratica"));
+echo PHP_EOL;
+print_r( explode(' , ', "teste, onte, pratica"));
+echo PHP_EOL;
+print_r( explode('e', "teste, onte, pratica"));
+echo PHP_EOL;
 ?>
