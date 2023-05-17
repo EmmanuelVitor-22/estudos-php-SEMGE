043ddd8bdc9bb9acc317114601e05a35c82801ff
Emmanuel Vitor <emmanuelvitor.ti@gmail.com>
2023-05-17 12:01:26 -0300

    feat: cria funções de ordenação de valores de forma crescente e decrescente

--- base/array/funcoes_de_array3.php
+++ base/array/funcoes_de_array3.php
@@ -6,6 +6,27 @@ $arr = [
     "maça"
 ];
 
+
+$arr2 = [
+    "uva",
+    "goiaba",
+    "maça", 
+    "pera"
+];
+
+$arr3 = [
+    "uva",
+    "goiaba",
+    "pera",
+    "maça" 
+];
+
+$arr4 = [
+    "uva",
+    "goiaba",
+    "pera",
+    "maça" 
+];
 $clientes = [
     "123.456.789-10" => [
         "nome" => "João da Silva",
@@ -16,5 +37,35 @@ $clientes = [
         "telefone" => "(22) 9876-5432"
     ]
 ];
+
+#ordenar array de forma crescente
+#ela ordena o valor mas altera as chaves
+print_r( $arr);
+echo PHP_EOL ;
+sort($arr);
+print_r( $arr);
+
+#ordenar array de forma crescente
+#ela ordena o valor e mantém as chaves
+print_r( $arr2);
+echo PHP_EOL ;
+asort($arr2);
+print_r( $arr2);
+
+
+#ordenar array de forma decrescente
+#ela ordena o valor mas altera as chaves
+print_r( $arr3);
+echo PHP_EOL ;
+rsort($arr3);
+print_r( $arr3);
+
+#ordenar array de forma decrescente
+#ela ordena o valor e mantém as chaves
+print_r( $arr4);
+echo PHP_EOL ;
+arsort($arr4);
+print_r( $arr4);
+
 
 ?>
