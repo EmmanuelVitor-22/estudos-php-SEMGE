421218fed36853b2a355e9c3b740ff0ec8fa68c0
Emmanuel Vitor <emmanuelvitor.ti@gmail.com>
2023-05-22 11:25:11 -0300

    feat: cria arquivo com exemplos de manipulação de arrow functions

--- funcoes_anonimas_e_arrow/arrow_function.php
+++ funcoes_anonimas_e_arrow/arrow_function.php
@@ -16,6 +16,45 @@
     echo PHP_EOL;
     echo "Usando uma arrow function atribuida a uma variavel;
     \n\n";
-    
+
     $filtrados = array_filter($numeros, fn($item)=> $item%2 == 0 );
+
+
+    echo PHP_EOL;
+    echo "Trabalhando com ARROW FUNCTIONS utilizando em array: \n\n";
+
+    $arrowFunctions = [
+        "apenasNumero" => fn($txt)=> (preg_replace('/\D/', '', $txt)),
+        "apenasMaiusculas" => fn($txt) => preg_replace('/[^A-Z]/', '',$txt),
+        "apenasMinusculas" => fn($txt) => preg_replace('/[^a-z]/', '',$txt)
+
+    ];
+
+    print_r($arrowFunctions["apenasNumero"]("Ano 2022"));
+    echo PHP_EOL;
+    print_r($arrowFunctions["apenasMaiusculas"]("Ano 2022"));
+    echo PHP_EOL;
+    print_r($arrowFunctions["apenasMinusculas"]("Ano 2022"));
+    echo PHP_EOL;
+
+    echo "Trabalhando com funções nomeadar que tem como retorno uma ARROW FUNCTIONS: \n\n";
+
+    function pegarFuncoesArrow($opcao){
+        if ($opcao == 1 ) {
+            return fn($txt)=> (preg_replace('/\D/', '', $txt));
+        } elseif ($opcao == 2) {
+            return fn($txt) => preg_replace('/[^A-Z]/', '',$txt); 
+        } else {
+            return fn($txt) => preg_replace('/[^a-z]/', '',$txt);
+        }   
+    }
+
+    echo pegarFuncoesArrow(1)("Lana 234");
+    echo PHP_EOL;
+    echo pegarFuncoesArrow(2)("Lana 234");
+    echo PHP_EOL;
+    echo pegarFuncoesArrow(3)("Lana 234");
+    echo PHP_EOL;
+
+    echo PHP_EOL;
 ?>
