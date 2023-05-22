b76dd7899d3f527013e146d401c528738f5e6162
Emmanuel Vitor <emmanuelvitor.ti@gmail.com>
2023-05-22 10:45:06 -0300

    feat:cria explicação sobre funções anonimas com funcoes nomedas

--- funcoes_anonimas_e_arrow/funcoes_anonimas2.php
+++ funcoes_anonimas_e_arrow/funcoes_anonimas2.php
@@ -3,10 +3,56 @@
     echo PHP_EOL;
     echo "Trabalhando com funções anonimas, diretamente e utilizando em array: \n\n";
 
+    echo "Trabalhando com funções anonimas, diretamente: \n\n";
+    $formatarTexto = function($texto){
+        return mb_strtoupper($texto);
+    };
+
+    print_r($formatarTexto("Luna"));
+
+    echo PHP_EOL;
+
+    echo "Trabalhando com funções anonimas utilizando em array: \n\n";
+
+    $funcoes = [
+        "upperCase" => function($txt){
+            return mb_strtoupper($txt);
+        },
+        "lowerCase" => function($txt){
+            return mb_strtolower($txt);
+        },
+        "hideText" => function($txt){
+            return str_pad("",strlen($txt), "?");
+        }
+    ];
+
+    print_r($funcoes["upperCase"]("teste"));
+    echo PHP_EOL;
+    print_r($funcoes["lowerCase"]("teste"));
+    echo PHP_EOL;
+    print_r($funcoes["hideText"]("teste"));
+    echo PHP_EOL;
+
+    echo "Trabalhando com funções nomeadar que tem como retorno uma função anônima: \n\n";
+
+    function pegarFuncoes($opcao){
+        if ($opcao == 1 ) {
+            return function($txt){
+                return mb_strtoupper($txt);
+            };
+        } elseif ($opcao == 2) {
+            return function($txt){
+                mb_strtolower($txt);
+            }; 
+        } else {
+            return function($txt){
+                return str_pad("",strlen($txt), "?");
+        };
+    }
+}
     
 
-
-
+echo pegarFuncoes(1)("Lana");
 
 
 
