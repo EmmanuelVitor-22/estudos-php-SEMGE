4ee2b34e0d4b1a5caeb2e35aea3e5ecc7dcbc263
Emmanuel Vitor <emmanuelvitor.ti@gmail.com>
2023-05-22 10:07:49 -0300

    feat: explica mais sobre manipulação de funções anonimas

---
+++ funcoes_anonimas_e_arrow/funcoes_anonimas2.php
@@ -0,0 +1,21 @@
+<?php 
+    echo "TRABALHANDO COM FUNÇÕES ANONIMAS PT: II";
+    echo PHP_EOL;
+    echo "Trabalhando com funções anonimas, diretamente e utilizando em array: \n\n";
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
