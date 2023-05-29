b8c6b01340720553cf05c7d131dbdd57e151196e
Emmanuel Vitor <emmanuelvitor.ti@gmail.com>
2023-05-29 09:43:52 -0300

    fix: corrige falta de validação

--- teste/enviar.php
+++ teste/enviar.php
@@ -4,17 +4,45 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <title>Resultado</title>
 </head>
 <body>
     <?php 
-    $nome = $_POST["nome"];
-    $email = $_POST["email"];
-    $msg = $_POST["mensagem"];
-
+    
+    if ((isset($nome) && !empty($nome)) && (isset($msg) && !empty($msg))){
+        $nome = $_POST["nome"];
+        $email = $_POST["email"];
+        $msg = $_POST["mensagem"];
+    }
     ?>
-    <p>Nome cadastrado: <?= $nome?></p>
-    <p>Email cadastrado: <?= $email?></p>
-    <p>Mensagem cadastrada: <?= $msg?></p>
+
+    <?php 
+        if(  (!empty($nome) && !empty($msg))) {
+            if ($nome === "Emmanuel" && ($email === "e@gmail.com")) {  
+                echo "<div class=\"wrapper\" >";
+                    echo "<div class=\"result\" >";
+                        echo "<p class=\" p_result\">Nome cadastrado: <b> $nome</b></p>";
+                        echo "<p class=\" p_result\">Email cadastrado: <b> $email</b></p>";
+                        echo "<p class=\" p_result\">Mensagem cadastrada: <b> $msg </b></p>";
+                    echo "</div>";
+                echo "</div>";
+            }else{
+                echo  "<div class=\"result\" >";
+                    echo  "<p class=\" p_result\">usuario ou email não reconhecidos</p>";
+                echo "</div>";
+            }
+    }else{
+        echo  "<div class=\"result\" >";
+            echo "<p class=\" p_result\">Por favor, preencha os campos que são obrigatórios</p>";
+        echo "</div>";
+    }
+    
+    ?>
+
+
+<p>
+    <a href="javascript:history.go(-1)">voltar para pagina anterior </a>
+</p>
 </body>
 </html>
