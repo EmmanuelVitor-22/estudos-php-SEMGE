40bf383273e84d4d33562ede13b5958d682b4a3e
Emmanuel Vitor <emmanuelvitor.ti@gmail.com>
2023-05-29 10:44:20 -0300

    fix: corrige erro de validação

--- teste/enviar.php
+++ teste/enviar.php
@@ -9,40 +9,48 @@
 </head>
 <body>
     <?php 
-    
-    if ((isset($nome) && !empty($nome)) && (isset($msg) && !empty($msg))){
-        $nome = $_POST["nome"];
-        $email = $_POST["email"];
-        $msg = $_POST["mensagem"];
-    }
-    ?>
 
-    <?php 
-        if(  (!empty($nome) && !empty($msg))) {
-            if ($nome === "Emmanuel" && ($email === "e@gmail.com")) {  
-                echo "<div class=\"wrapper\" >";
-                    echo "<div class=\"result\" >";
-                        echo "<p class=\" p_result\">Nome cadastrado: <b> $nome</b></p>";
-                        echo "<p class=\" p_result\">Email cadastrado: <b> $email</b></p>";
-                        echo "<p class=\" p_result\">Mensagem cadastrada: <b> $msg </b></p>";
+  
+$nome = $_POST["nome"];
+$email = $_POST["email"];
+$msg = $_POST["mensagem"];
+
+if ((isset($nome) && !empty($nome)) && (isset($msg) && !empty($msg))){
+// $_SERVER["REQUEST_METHOD"]==="POST" a verificação acima pode ser escrita assim também
+    
+    
+            if(!empty($nome) && !empty($msg)) {
+
+                if ($nome == "Emmanuel" && $email == "emma@gmail.com" ) {  
+                    echo  "<div class=\"result\" >";
+                    echo "<p class=\" p_result\">Nome cadastrado: <b>$nome</b></p>";
+                            echo "<p class=\" p_result\">Email cadastrado: <b> $email</b></p>";
+                            echo "<p class=\" p_result\">Mensagem cadastrada: <b> $msg </b></p>";
+                    echo  " <p>
+                        <a href=\"javascript:history.go(-1)\">voltar para pagina anterior </a>
+                    </p>";
+                echo "</div>";
+                }else{
+                    echo  "<div class=\"result\" >";
+                        echo  "<p class=\" p_result\">usuario ou email não reconhecidos</p>";
+                        echo  " <p>
+                    <a href=\"javascript:history.go(-1)\">voltar para pagina anterior </a>
+                </p>";
                     echo "</div>";
-                echo "</div>";
-            }else{
-                echo  "<div class=\"result\" >";
-                    echo  "<p class=\" p_result\">usuario ou email não reconhecidos</p>";
-                echo "</div>";
-            }
-    }else{
-        echo  "<div class=\"result\" >";
-            echo "<p class=\" p_result\">Por favor, preencha os campos que são obrigatórios</p>";
-        echo "</div>";
+                }
+        }else{
+            echo  "<div class=\"result\" >";
+                echo "<p class=\" p_result\">Por favor, preencha os campos que são obrigatórios</p>";
+                echo  " <p>
+                    <a href=\"javascript:history.go(-1)\">voltar para pagina anterior </a>
+                </p>";
+            echo "</div>";
+        }
+                        
     }
-    
     ?>
 
 
-<p>
-    <a href="javascript:history.go(-1)">voltar para pagina anterior </a>
-</p>
+
 </body>
 </html>
