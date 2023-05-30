2c90291fbd124936f9e9fff6824ab5620827e929
Emmanuel Vitor <emmanuelvitor.ti@gmail.com>
2023-05-30 09:59:55 -0300

    feat: cria estudo sobre OOP

---
+++ OOP/introducao/src/Conta.php
@@ -0,0 +1,13 @@
+<?php 
+
+class Conta {
+    private $cpfTitular;
+    private $nome;
+    private $saldo;  
+
+    
+}
+$conta = new Conta();
+var_dump($conta);
+
+?>
---
+++ OOP/introducao/src/teoria.php
@@ -0,0 +1,10 @@
+Classe:
+ - uma forma/molde para criação de objetos.
+ - ao criar uma classe está criando um novo tipo especifico, diferente dos primitivos (ex: int, string).
+ 
+Objetos : 
+ - O mesmo que uma instância de uma classe
+
+ Abstração: 
+  - Conceito do mundo real, onde, dentro do sistema só serão utilizados (abstraido para dentro do sistema) somente o que é nescessário para ele. Mesmo que no mundo real exista diversos outros atributos. 
+ 
