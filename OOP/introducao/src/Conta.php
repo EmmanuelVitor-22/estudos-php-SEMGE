c7188879920546631cff7f857ea1a48aa22276f2
Emmanuel Vitor <emmanuelvitor.ti@gmail.com>
2023-06-01 08:05:50 -0300

    limpa CCCCcódigo

--- OOP/introducao/src/Conta.php
+++ OOP/introducao/src/Conta.php
@@ -25,6 +25,7 @@ $conta = new Conta("040428", "Emm", 10000);
 $conta->sacar(33);
 var_dump($conta);
 $conta2 = new Conta("8777", "sdsds", 230);
+$conta3 = new Conta("ssdsd", "sdfd", 90);
 /* 
  * $primeiraConta = new Conta("ffdfdf", "Nome ", 1234);
  * * $primeiraConta = armazena o endereço para onde o objeto está armazenado em memoria e não o objeto em si;
