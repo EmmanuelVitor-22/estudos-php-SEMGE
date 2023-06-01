e64d0e51486cc1042806c7f4c54746f92b9e63d4
Emmanuel Vitor <emmanuelvitor.ti@gmail.com>
2023-06-01 12:02:49 -0300

    feat: cria validação de nome  utiliza no construtor

--- OOP/introducao/src/Conta.php
+++ OOP/introducao/src/Conta.php
@@ -1,70 +1,62 @@
 <?php
-class Conta
-{
+class Conta{
     //modificadores de acesso (private, protected, public)
     private $cpfTitular;
     private $nome;
     private $saldo = 0;
 
     //metodo construtor
-    public function __construct($cpfTitular, $nome)
-    {
+    public function __construct($cpfTitular, $nome){
         $this->cpfTitular = $cpfTitular;
-        $this->nome = $nome;
+        $this->validaNome( $nome) ;
         $this->saldo = 0;
     }
 
     //metodos
     public function sacar($valorDeSaque){
-        if ($valorDeSaque < 0 || $valorDeSaque > $this->saldo ) 
-            return print( "Valor invalido ou saldo insuficiente. \n");
-      
-            return $this->saldo -= $valorDeSaque;
-            
-        
+         ($valorDeSaque < 0 || $valorDeSaque > $this->saldo ) 
+            ?print( "Valor invalido ou saldo insuficiente. \n")
+            :$this->saldo -= $valorDeSaque;
     }
+
     public function depositar(float $valorDeDeposito){
         ($valorDeDeposito<0) ? print( "Valor de deposito não pode ser negativo. \n")
                              : $this->saldo +=$valorDeDeposito;
-        
     }
 
     public function tranferir(Conta $contaDestino, $valorDeTranferencia){
-      ($valorDeTranferencia>$this->saldo) ? print( "Valor indisponivel. \n")
-                                          :
-                                            $this->sacar($valorDeTranferencia);
-                                            $contaDestino->depositar($valorDeTranferencia);
-        
+      ($valorDeTranferencia>$this->saldo) 
+      ? print( "Valor indisponivel. \n")
+      : $this->sacar($valorDeTranferencia);
+        $contaDestino->depositar($valorDeTranferencia); 
     }
 
+    /* Metodos privados: Utilizado quando temos regras que cabem somente a classe
+       e não tem a nescessidade de ser exposta ao "mundo exterior" */
+
+    private function validaNome($nome){
+        if (strlen($nome)<5) {
+            echo "Nome invalido. \n Se nome é menor que 5 letras ponha seu sobrenome.";
+        }
+        return $nome;
+    }
     /*
         Metodos GET - Devolvem o valor (retornam) de um atributo
         Metodos Set - definem um valor de um atributo.
                     - Podem conter uma validação (ou comportamento).  
     */
-
-
-    public function getCpfTitular()
-    {
+    public function getCpfTitular(){
         return $this->cpfTitular;
     }
- 
-    public function setCpfTitular($cpfTitular)
-    {
+    public function setCpfTitular($cpfTitular){
         $this->cpfTitular = $cpfTitular;
 
         return $this;
     }
-
-    
-    public function getNome()
-    {
+    public function getNome(){
         return $this->nome;
     }
-
-     
-    public function setNome($nome)
-    {
+    public function setNome($nome){
         $this->nome = $nome;
 
         return $this;
@@ -76,9 +68,7 @@ class Conta
         Metodos de acesso não precisam, obrigatóriamente, serem criados para todos
         os atributos
     */ 
-    
-    public function getSaldo()
-    {
+    public function getSaldo(){
         return $this->saldo;
     }
 
